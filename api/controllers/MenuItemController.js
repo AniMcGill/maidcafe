3136daf8630688e52ff5feb1fe39738ee42ed73f
Natsumi Hoshino <me@kagehoshi.com>
2015-01-26 15:29:09 -0500

    refactored controllers, using native queries to aggregate

--- api/controllers/MenuItemController.js
+++ api/controllers/MenuItemController.js
@@ -43,48 +43,22 @@ module.exports = {
    * `MenuItemController.main()`
    */
   main: function (req, res) {
-    async.auto({
-      entrees: function(next){
-        MenuItem.find({ category: 'entree' }).exec(next);
-      },
-      mains: function(next){
-        MenuItem.find({ category: 'main' }).exec(next);
-      },
-      sides: function(next){
-        MenuItem.find({ category: 'side' }).exec(next);
-      },
-      deserts: function(next){
-        MenuItem.find({ category: 'desert' }).exec(next);
-      },
-      drinks: function(next){
-        MenuItem.find({ category: 'drink' }).exec(next);
-      }
-    },
-      function allDone(err, async_data){
-        if(err) return res.serverError(err);
-
-        return res.view({
-          menuitems: [{
-            name: 'Entree',
-            data: async_data.entrees
-          },
-          {
-            name: 'Main',
-            data: async_data.mains
-          },
-          {
-            name: 'Side',
-            data: async_data.sides
-          },
-          {
-            name: 'Desert',
-            data: async_data.deserts
-          },
-          {
-            name: 'Drink',
-            data: async_data.drinks
-          }]
-        });
+    MenuItem.native(function(err, collection){
+      if (err) return res.serverError(err);
+      collection.aggregate(
+        [{
+            $group: {
+              _id: "$category",
+              items: { $push: "$$ROOT"}
+            }
+        }],
+        function(err, menuitems){
+          if (err) return res.serverError(err);
+          return res.view({
+            menuitems: menuitems
+          });
+        }
+      );
     });
 
   }
--- api/controllers/OrderController.js
+++ api/controllers/OrderController.js
@@ -14,54 +14,29 @@ module.exports = {
    */
   create: function (req, res) {
     if(req.method == 'GET') {
-      async.auto({
-          entrees: function(next){
-            MenuItem.find({ category: 'entree' }).exec(next);
-          },
-          mains: function(next){
-            MenuItem.find({ category: 'main' }).exec(next);
-          },
-          sides: function(next){
-            MenuItem.find({ category: 'side' }).exec(next);
-          },
-          deserts: function(next){
-            MenuItem.find({ category: 'desert' }).exec(next);
-          },
-          drinks: function(next){
-            MenuItem.find({ category: 'drink' }).exec(next);
-          },
-          customers: function(next){
-            Customer.find({ table: req.param('table') }).exec(next);
-          }
-        },
-        function allDone(err, async_data){
-          if(err) return res.serverError(err);
+      MenuItem.native(function(err, collection){
+        if (err) return res.serverError(err);
+        collection.aggregate(
+          [{
+            $group: {
+              _id: "$category",
+              items: { $push: "$$ROOT"}
+            }
+          }],
+          function(err, menuitems){
+            if (err) return res.serverError(err);
+            Customer.find({ table: req.param('table') }).exec(function(err, customers){
+              if (err) return res.serverError(err);
 
-          return res.view({
-            table: req.param('table'),
-            customers: async_data.customers,
-            menuitems: [{
-              name: 'Entree',
-              data: async_data.entrees
-            },
-              {
-                name: 'Main',
-                data: async_data.mains
-              },
-              {
-                name: 'Side',
-                data: async_data.sides
-              },
-              {
-                name: 'Desert',
-                data: async_data.deserts
-              },
-              {
-                name: 'Drink',
-                data: async_data.drinks
-              }]
-          });
-        });
+              return res.view({
+                table: req.param('table'),
+                customers: customers,
+                menuitems: menuitems
+              });
+            });
+          }
+        );
+      });
     }
     else {
       if(req.body.orders instanceof Array){
@@ -108,7 +83,7 @@ module.exports = {
    * Show unpaid orders and their current state
    */
   main: function (req, res) {
-    Order.find({where: {paid: false} }).populate('customer').populate('menuItem').exec(function(err, orders){
+    Order.find({where: {paid: false}, sort: 'createdAt ASC' }).populate('customer').populate('menuItem').exec(function(err, orders){
       if(err) return res.serverError(err);
       return res.view({
         orders: orders
